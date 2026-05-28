8dc62e626381bac191c74d73b4c2a8670884052b
Femiel <femielp@gmail.com>
2026-05-28 12:22:51 +0100

    Adicionei notificacao e services no Angular

--- NzolaSPA/src/app/app.routes.ts
+++ NzolaSPA/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { HOME_ROUTES } from './modules/home/home.routes';
 export const routes: Routes = [
   /*{ path: '',   loadChildren: () => import('./modules/home/home.routes').then((m) => m.HOME_ROUTES) },
   { path: 'home', loadChildren: () => import('./modules/home/home.routes').then((m) => m.HOME_ROUTES) }, 
-  */{
+*/{
     path: 'feed',
     loadComponent: () =>
       import('./modules/feed/feed').then((m) => m.Feed),
