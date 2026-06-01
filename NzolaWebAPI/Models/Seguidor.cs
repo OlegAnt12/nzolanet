11385f80e634a66997891d5b8b01a9c88716fb98
Femiel <femielp@gmail.com>
2026-06-01 15:09:38 +0100

    Adiciona model Seguidor

---
+++ NzolaWebAPI/Models/Seguidor.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NzolaWebAPI.Models
+{
+    public class Seguidor
+    {
+         public int Id { get; set; }
+
+        public int SeguidorId { get; set; }
+
+        public int SeguidoId { get; set; }
+
+        public DateTime DataInicio { get; set; } = DateTime.Now;
+    }
+}
