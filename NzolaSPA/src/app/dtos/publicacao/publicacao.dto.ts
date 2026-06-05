1f667565542e6a88d09208870679fc48fbc04017
unknown <pjpegado01@gmail.com>
2026-06-05 11:32:34 +0100

    resolvido erro sobre outdir

--- NzolaSPA/src/app/dtos/publicacao/publicacao.dto.ts
+++ NzolaSPA/src/app/dtos/publicacao/publicacao.dto.ts
@@ -1,2 +1,16 @@
 export class PublicacaoDto {
+  id: number = 0;
+  utilizadorId: number = 0;
+  nomeAutor: string = '';
+  fotoAutor: string = '';
+  texto: string = '';
+  imagemUrl: string = '';
+  videoUrl: string = '';
+  dataPublicacao: string = '';
+  numeroBazes: number = 0;
+  numeroComentarios: number = 0;
+  utilizadorJaDeuBaze: boolean = false;
+  bazeId: number | null = null;
 }
+
+
